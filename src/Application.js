9c3fb08155686b61f8994afd6d98d8c01a02efe0
metroid112 <federico.aguilera112@gmail.com>
2019-11-04 18:12:45 -0300

    Part 5

--- src/Application.js
+++ src/Application.js
@@ -1,8 +1,33 @@
 import React, {Component} from 'react';
 
 class Application extends Component {
-    render() {
-        return "Hello, world";    
+    constructor(props){
+        super(props);
+
+        this.state = {
+            count: 0
+        }
+    }
+
+    handleClick = () => {
+        this.setState({count: this.state.count + 1});
+    }
+    
+    componentDidUpdate(props, state){
+        console.log("Updated from", state, "to", this.state);
+    }
+
+    render(){
+        let {count} = this.state;
+
+        return (
+            <div>
+                <h1>You clicked the button {count} times</h1>
+                <span>
+                    <button onClick={() => this.handleClick()}>Click Me</button>
+                </span>
+            </div>
+        );   
     }
 }
 
