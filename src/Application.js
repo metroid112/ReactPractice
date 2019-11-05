b55925b606f2127b1edc2f86d5366d211b722a85
metroid112 <federico.aguilera112@gmail.com>
2019-11-05 17:44:32 -0300

    Part 7

--- src/Application.js
+++ src/Application.js
@@ -1,11 +1,13 @@
 import React, {Component} from 'react';
+import HighScore from './HighScore';
 
 class Application extends Component {
     constructor(props){
         super(props);
 
         this.state = {
-            count: 0
+            count: 0,
+            overTen: false
         }
     }
 
@@ -14,7 +16,16 @@ class Application extends Component {
     }
     
     componentDidUpdate(props, state){
-        console.log("Updated from", state, "to", this.state);
+        if(this.state.count > 10 && this.state.count !== state.count &&  !this.state.overTen){
+            this.setState({overTen: true});
+        }
+    }
+
+    resetCount = (e) => {
+        this.setState({
+            count: 0,
+            overTen: false
+        });
     }
 
     render(){
@@ -23,6 +34,10 @@ class Application extends Component {
         return (
             <div>
                 <h1>You clicked the button {count} times</h1>
+                <HighScore 
+                    overTen={this.state.overTen} 
+                    onReset={this.resetCount}
+                />
                 <span>
                     <button onClick={() => this.handleClick()}>Click Me</button>
                 </span>
